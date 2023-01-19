22f5cad1f70c9c404b5ddd627ed72f48609703ad
hawkobe <sanders.jms@gmail.com>
2023-01-19 15:30:31 -0700

    began clean phone method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,14 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  # If the phone number is less than 10 digits, assume that it is a bad number
+  # If the phone number is 10 digits, assume that it is good
+  # If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
+  # If the phone number is 11 digits and the first number is not 1, then it is a bad number
+  # If the phone number is more than 11 digits, assume that it is a bad number
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -43,13 +51,26 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+i = 0
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+  phone_number = row[:homephone]
 
-  save_thank_you_letter(id, form_letter)
+  no_format = phone_number.gsub(/[^0-9]/, "")
+
+  if no_format.length == 10
+    puts "#{i} #{no_format.insert(0, '(').insert(4, ')').insert(8, '-')}"
+    i += 1
+  elsif no_format.length == 11 && no_format[0] == "1"
+    puts "#{i} #{no_format[1..10].insert(0, '(').insert(4, ')').insert(8, '-')}"
+    i += 1
+  end
+  # form_letter = erb_template.result(binding)
+
+  # save_thank_you_letter(id, form_letter)
 end
